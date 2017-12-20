ff93a89138dea4460238f3940e19d80892b8b869
FabioCruz20 <fabiovict95@gmail.com>
2017-12-20 13:17:20 -0200

    Teste de escrita de objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
 
+class Conteiner {
+  dados: Dados;
+}
+
+class Dados {
+  score: any;
+}
+
+
+class Ordinal  {
+
+  public name: string;
+  public value: string;
+
+  constructor(name: string, value: string) {
+    this.name = name;
+    this.value = value;
+  }
+
+  toString() {
+    return this.value;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +31,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  o1: Ordinal = new Ordinal('teste', '987654321'); // chama toString() sobrescrito
+
+  // não chamam toString() sobrescrito
+  o2: Ordinal = {name: 'teste2', value: '1000'};
+
+  // função hardCoded
+  o3: Ordinal = {name: 'teste3', value: '2000', toString: function() {return this.value; } };
+
+  // valor literal escrito normalmente
+  c1: Conteiner = {dados: {score: 1}};
+
+  // o valor score é um objeto Ordinal. Seu toString() não é chamado.
+  c2: Conteiner = {dados: {score: {name: 'teste', value: '2'}}};
+
+  // Atribuindo um objeto Ordinal criado na sintaxe do new Ordinal()
+  c3: Conteiner = {dados: {score: this.o1}};
 }
